2101fe1d23a32d2c5b90ba3bcf908c1a3dc986c2
Cheng Tong <mrchengtong@163.com>
2020-12-13 11:21:55 +0800

    6 ZigZag Conversion

---
+++ src/Algorithms/ZigZagConversion.java
@@ -0,0 +1,47 @@
+package Algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 6. ZigZag Conversion
+ * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ *
+ * And then read line by line: "PAHNAPLSIIGYIR"
+ *
+ * Write the code that will take a string and make this conversion given a number of rows:
+ * string convert(string s, int numRows);
+ *
+ * Example 1:
+ *
+ * Input: s = "PAYPALISHIRING", numRows = 3
+ * Output: "PAHNAPLSIIGYIR"
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/zigzag-conversion
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class ZigZagConversion {
+    public String convert(String s, int numRows) {
+        if (numRows < 2) return s;
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i=0; i<numRows; i++) {
+            rows.add(new StringBuilder());
+
+        }
+        int i = 0, flag = -1;
+        for (char c : s.toCharArray()) {
+            rows.get(i).append(c);
+            if (i == 0 || i == numRows - 1) flag = -flag;
+            i += flag;
+        }
+        StringBuilder res = new StringBuilder();
+        for (StringBuilder row : rows) {
+            res.append(row);
+        }
+        return res.toString();
+    }
+}
