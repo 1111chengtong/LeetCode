2c85ffd36967cc4f194e693a281276aa3949e65b
Cheng Tong <mrchengtong@163.com>
2021-03-22 16:12:10 +0800

    提交了1道题

---
+++ src/Algorithms/MaxAreaOfIsland.java
@@ -0,0 +1,66 @@
+package Algorithms;
+
+/**
+ * Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+ *
+ * Find the maximum area of an island in the given 2D array. (If there is no island, the maximum area is 0.)
+ *
+ * Example 1:
+ *
+ * [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ *  [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ *  [0,1,1,0,1,0,0,0,0,0,0,0,0],
+ *  [0,1,0,0,1,1,0,0,1,0,1,0,0],
+ *  [0,1,0,0,1,1,0,0,1,1,1,0,0],
+ *  [0,0,0,0,0,0,0,0,0,0,1,0,0],
+ *  [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ *  [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+ * Given the above grid, return 6. Note the answer is not 11, because the island must be connected 4-directionally.
+ * Example 2:
+ *
+ * [[0,0,0,0,0,0,0,0]]
+ * Given the above grid, return 0.
+ * Note: The length of each dimension in the given grid does not exceed 50.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/max-area-of-island
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MaxAreaOfIsland {
+
+    public int maxAreaOfIsland(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int m = grid.length;
+        int n = grid[0].length;
+        int maxArea = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                int localArea = dfs(grid, i, j);
+                maxArea = Math.max(maxArea, localArea);
+            }
+        }
+        return maxArea;
+
+    }
+
+    int[] direction = new int[] {-1, 0, 1, 0, -1};
+
+    public int dfs(int[][] grid, int i, int j) {
+        if (grid[i][j] == 0) {
+            return 0;
+        }
+        int area = 1;
+        grid[i][j] = 0;
+        for (int k = 0; k < 4; k++) {
+            int x = i + direction[k];
+            int y = j + direction[k+1];
+            if (x >= 0 && x < grid.length && y >= 0 && y < grid[0].length) {
+                area += dfs(grid, x, y);
+            }
+        }
+        return area;
+    }
+}
