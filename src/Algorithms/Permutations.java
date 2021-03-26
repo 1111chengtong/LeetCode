dec10b122c053bd2a266c0f64cd2443261b74f64
Cheng Tong <mrchengtong@163.com>
2021-03-27 00:09:40 +0800

    提交了1道题

---
+++ src/Algorithms/Permutations.java
@@ -0,0 +1,62 @@
+package Algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+ *
+ *  
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,2,3]
+ * Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ * Example 2:
+ *
+ * Input: nums = [0,1]
+ * Output: [[0,1],[1,0]]
+ * Example 3:
+ *
+ * Input: nums = [1]
+ * Output: [[1]]
+ *  
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 6
+ * -10 <= nums[i] <= 10
+ * All the integers of nums are unique.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/permutations
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Permutations {
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return res;
+        }
+        List<Integer> track = new ArrayList<>();
+        backtrack(nums, res, track);
+        return res;
+
+    }
+    public void backtrack(int[] nums, List<List<Integer>> res, List<Integer> track) {
+        int size = track.size();
+        if (size == nums.length) {
+            res.add(new ArrayList<Integer>(track));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (track.contains(nums[i])) {
+                continue;
+            }
+            track.add(nums[i]);
+            backtrack(nums, res, track);
+            track.remove(size);
+        }
+    }
+}
