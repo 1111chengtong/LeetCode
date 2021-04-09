2c87e3956a4ad18d376fe50bf116eb53da98a2d1
chengtong <mrchengtong@163.com>
2021-04-09 16:49:44 +0800

    '提交了3道题'

---
+++ src/Algorithms/MaximalSquare.java
@@ -0,0 +1,59 @@
+package Algorithms;
+
+/**
+ * Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+ *
+ *  
+ *
+ * Example 1:
+ *
+ *
+ * Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+ * Output: 4
+ * Example 2:
+ *
+ *
+ * Input: matrix = [["0","1"],["1","0"]]
+ * Output: 1
+ * Example 3:
+ *
+ * Input: matrix = [["0"]]
+ * Output: 0
+ *  
+ *
+ * Constraints:
+ *
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 300
+ * matrix[i][j] is '0' or '1'.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximal-square
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MaximalSquare {
+
+    public int maximalSquare(char[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        int maxSide = 0;
+        int[][] dp = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    }
+                    maxSide = Math.max(maxSide, dp[i][j]);
+                }
+            }
+        }
+        return maxSide * maxSide;
+    }
+}
