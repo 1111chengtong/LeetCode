a4f3cdc1f05f9dc05c8da5392abda830917813af
chengtong <mrchengtong@163.com>
2021-04-17 19:42:38 +0800

    '提交了2道题'

---
+++ src/Algorithms/WordBreak.java
@@ -0,0 +1,60 @@
+package Algorithms;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+ *
+ * Note that the same word in the dictionary may be reused multiple times in the segmentation.
+ *
+ *  
+ *
+ * Example 1:
+ *
+ * Input: s = "leetcode", wordDict = ["leet","code"]
+ * Output: true
+ * Explanation: Return true because "leetcode" can be segmented as "leet code".
+ * Example 2:
+ *
+ * Input: s = "applepenapple", wordDict = ["apple","pen"]
+ * Output: true
+ * Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+ * Note that you are allowed to reuse a dictionary word.
+ * Example 3:
+ *
+ * Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+ * Output: false
+ *  
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 300
+ * 1 <= wordDict.length <= 1000
+ * 1 <= wordDict[i].length <= 20
+ * s and wordDict[i] consist of only lowercase English letters.
+ * All the strings of wordDict are unique.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/word-break
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class WordBreak {
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordSet = new HashSet<>(wordDict);
+        int len = s.length();
+        boolean[] dp = new boolean[len + 1];
+        dp[0] = true;
+        for (int i = 1; i <= len; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[len];
+    }
+}
