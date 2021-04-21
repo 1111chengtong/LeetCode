c229682625df06ae17481c56917b4a871bf2282f
chengtong <mrchengtong@163.com>
2021-04-21 12:00:25 +0800

    '提交了1道题'

---
+++ src/Algorithms/LongestIncreasingSubsequence.java
@@ -0,0 +1,57 @@
+package Algorithms;
+
+/**
+ * Given an integer array nums, return the length of the longest strictly increasing subsequence.
+ *
+ * A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the remaining elements. For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+ *
+ *  
+ *
+ * Example 1:
+ *
+ * Input: nums = [10,9,2,5,3,7,101,18]
+ * Output: 4
+ * Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+ * Example 2:
+ *
+ * Input: nums = [0,1,0,3,2,3]
+ * Output: 4
+ * Example 3:
+ *
+ * Input: nums = [7,7,7,7,7,7,7]
+ * Output: 1
+ *  
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 2500
+ * -104 <= nums[i] <= 104
+ *  
+ *
+ * Follow up:
+ *
+ * Could you come up with the O(n2) solution?
+ * Could you improve it to O(n log(n)) time complexity?
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-increasing-subsequence
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LongestIncreasingSubsequence {
+
+    public int lengthOfLIS(int[] nums) {
+        int len = nums.length;
+        int[] dp = new int[len];
+        int maxLength = 1;
+        for (int i = 0; i < len; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLength = Math.max(maxLength, dp[i]);
+        }
+        return maxLength;
+    }
+}
