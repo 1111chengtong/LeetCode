7417fc29ff4ce111db54d667e66b088eeb492551
chengtong <mrchengtong@163.com>
2021-05-07 18:44:55 +0800

    '提交了3道题'

---
+++ src/Algorithms/EditDistance.java
@@ -0,0 +1,68 @@
+package Algorithms;
+
+/**
+ * Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+ *
+ * You have the following three operations permitted on a word:
+ *
+ * Insert a character
+ * Delete a character
+ * Replace a character
+ *  
+ *
+ * Example 1:
+ *
+ * Input: word1 = "horse", word2 = "ros"
+ * Output: 3
+ * Explanation:
+ * horse -> rorse (replace 'h' with 'r')
+ * rorse -> rose (remove 'r')
+ * rose -> ros (remove 'e')
+ * Example 2:
+ *
+ * Input: word1 = "intention", word2 = "execution"
+ * Output: 5
+ * Explanation:
+ * intention -> inention (remove 't')
+ * inention -> enention (replace 'i' with 'e')
+ * enention -> exention (replace 'n' with 'x')
+ * exention -> exection (replace 'n' with 'c')
+ * exection -> execution (insert 'u')
+ *  
+ *
+ * Constraints:
+ *
+ * 0 <= word1.length, word2.length <= 500
+ * word1 and word2 consist of lowercase English letters.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/edit-distance
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class EditDistance {
+
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        if (n == 0 || m == 0) {
+            return n + m;
+        }
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j < m + 1; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = Math.min(dp[i - 1][j] + 1, Math.min(dp[i][j - 1] + 1, dp[i - 1][j - 1]));
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j] + 1, Math.min(dp[i][j - 1] + 1, dp[i - 1][j - 1] + 1));
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
